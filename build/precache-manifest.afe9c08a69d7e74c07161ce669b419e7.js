2619e726d94eca4537b9cdce9a91bad0b94c1e05
Alex <AlexCarterDeveloper@gmail.com>
2018-12-31 03:54:07 +0300

    Add

---
+++ build/precache-manifest.afe9c08a69d7e74c07161ce669b419e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "99614b47c31fd8353c54",
+    "url": "/static/js/main.99614b47.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "99614b47c31fd8353c54",
+    "url": "/static/css/main.6db354e4.chunk.css"
+  },
+  {
+    "revision": "aa3ee25817577d0f62daa2e95984ce50",
+    "url": "/index.html"
+  }
+];
